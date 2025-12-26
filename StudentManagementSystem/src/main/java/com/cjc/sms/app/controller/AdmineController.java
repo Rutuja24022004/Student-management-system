dc7cf5dea10d5871f131c7532afc5d16aa1b54bd
Rpawar24022004 <pawarrutuja323@gmail.com>
2025-12-26 16:37:33 +0530

    second and third day work

--- StudentManagementSystem/src/main/java/com/cjc/sms/app/controller/AdmineController.java
+++ StudentManagementSystem/src/main/java/com/cjc/sms/app/controller/AdmineController.java
@@ -1,17 +1,65 @@
 package com.cjc.sms.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cjc.sms.app.model.Student;
+import com.cjc.sms.app.serviceI.StudentServiceI;
 
 @Controller
 public class AdmineController {
 	
-	
+	@Autowired
+   StudentServiceI s;
     @RequestMapping("/")
 	public String prelogin()
 	{
-		return "success";
+		return "login";
 	}
-	
+    
+    @RequestMapping("/login")
+    public String onlogin(@RequestParam String username , @RequestParam String password,Model m)
+    {
+    	if(username.equals("Admin") && password.equals("Admin123"))
+    	{
+    		
+    		List<Student> sts=s.getAllStudent();
+    		m.addAttribute("data", sts);
+    		return "adminscreen";
+    	}
+    	else
+    	{
+    		m.addAttribute("login_fail","Enter valid login details.");
+    		
+    		return "login";
+    	}
+    	
+    }
+    @RequestMapping("enroll_student")
+    public String saveStudent(@ModelAttribute Student student,Model m)
+    {
+    	s.saveStudentDetailed(student);
+    	List<Student> st = s.getAllStudent();
+    	m.addAttribute("data", st);
+    	return "adminscreen";
+    }
+    
+    @RequestMapping("/delete")
+    public String removeStudent(@RequestParam("rollno") int rollno,Model m)
+    {
+    	s.deleteStudent(rollno);
+    	List<Student> st = s.getAllStudent();
+    	m.addAttribute("data", st);
+    	return "adminscreen";
+    }
+    
+    
+    
 
 }
